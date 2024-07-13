a23915aeb41b08bebd79ae28eecf68c3cd6e4662
oufkirhamza <oufkirhamza08@gmail.com>
2024-07-13 22:13:37 +0100

    fixing path prblm

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 
 import { Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/Home/home';
-import { AboutPage } from './pages/About/about';
 import { MyProvider } from './utils/contextProvider';
 import Header from './layouts/header';
 function App() {
